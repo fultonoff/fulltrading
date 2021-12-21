aa5c1e40f9f04276b4934463ad1820f430fe0ca2
fulton <fultonkilebe7@gmail.com>
2021-12-22 01:16:48 +0200

    first commit

---
+++ JavaScript/slider.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 3,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  autoplay: {
+    delay: 2500
+  }
+});
